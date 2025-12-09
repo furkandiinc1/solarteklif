f9a9f8aeb8a18a4084e5e24ab3cba4cf1573acf8
furkandiinc1 <furkandiinc1@gmail.com>
2025-12-09 21:25:26 +0300

    update battery descriptions to kW lifepo4

--- data.js
+++ data.js
@@ -6,7 +6,7 @@ const packages = {
     materials: [
       { name: "18x Kalyon Pv 550W M10 144 HC Bifacial G2G panel", price: 24000 },
       { name: "APX11 PRO Akıllı İnverter MPPT", price: 10000 },
-      { name: "2x 51,2V 100Ah LiFePO4 lityum akü", price: 8000 },
+      { name: "10 kW LiFePO4 lityum akü (51,2V sistem)", price: 8000 },
       { name: "Kablo, sigorta, AC/DC pano", price: 3000 },
       { name: "Kurulum ve test", price: 2000 }
     ]
@@ -17,7 +17,7 @@ const packages = {
     materials: [
       { name: "18x Kalyon Pv 550W M10 144 HC Bifacial G2G panel", price: 30000 },
       { name: "APX11 PRO Akıllı İnverter MPPT", price: 14000 },
-      { name: "3x 51,2V 100Ah LiFePO4 lityum akü", price: 9000 },
+      { name: "15 kW LiFePO4 lityum akü (51,2V sistem)", price: 9000 },
       { name: "Kablo, sigorta, AC/DC pano", price: 4000 },
       { name: "Kurulum, test, devreye alma", price: 2000 }
     ]
@@ -28,7 +28,7 @@ const packages = {
     materials: [
       { name: "18x Kalyon Pv 550W M10 144 HC Bifacial G2G panel", price: 40000 },
       { name: "APX11 PRO Akıllı İnverter MPPT", price: 16000 },
-      { name: "4x 51,2V 100Ah LiFePO4 lityum akü", price: 12000 },
+      { name: "20 kW LiFePO4 lityum akü (51,2V sistem)", price: 12000 },
       { name: "Kablo, sigorta, AC/DC pano", price: 5000 },
       { name: "Kurulum, test, devreye alma", price: 3000 }
     ]
